6fa5595f8ef3e92e7e4f160aea1b0d82e8915ce5
vaakian <vaakian@gmail.com>
2022-03-07 22:57:49 +0800

    打白工男孩上线 🚀

--- client/src/components/SubLoading.jsx
+++ client/src/components/SubLoading.jsx
@@ -1,11 +1,14 @@
+import { LoadingOutlined } from '@ant-design/icons'
+import { Alert } from 'antd'
 import './components.scss'
 /**
  * 仅在父元素中以100%大小展现加载动画
  */
 const SubLoading = ({ tip }) => {
   return (
-    <div className="subloading">
-      Loading……
+    <div className='subloading'>
+      <LoadingOutlined twoToneColor="#52c41a" />
+      <Alert message="数据加载中^.^" type="info" font showIcon />
     </div>
   )
 }
